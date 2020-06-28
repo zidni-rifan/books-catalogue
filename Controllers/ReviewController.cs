50279fef156b3438058a33303eba1f877d01f6e5
Zidni Rif'an <zidni.rifan7@gmail.com>
2020-06-28 20:35:19 +0700

    api endpoint review

--- Controllers/ReviewController.cs
+++ Controllers/ReviewController.cs
@@ -10,7 +10,7 @@ namespace BooksCatalogue.Controllers
 {
     public class ReviewController : Controller
     {
-        private string apiEndpoint = "https://bookscatalogueapi.azurewebsites.net/api/";
+        private string apiEndpoint = "https://bookscatalogueapi.azurewebsites.net/api/review/";
 
         public ReviewController() {
         }
