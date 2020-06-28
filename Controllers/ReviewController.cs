a080ec42b7799a9b75e0e2f5bc14e2587277f087
Zidni Rif'an <zidni.rifan7@gmail.com>
2020-06-28 09:54:14 +0700

    change url

--- Controllers/ReviewController.cs
+++ Controllers/ReviewController.cs
@@ -10,7 +10,7 @@ namespace BooksCatalogue.Controllers
 {
     public class ReviewController : Controller
     {
-        private string apiEndpoint = "https://bookscatalogueapi-dicoding.azurewebsites.net/api/";
+        private string apiEndpoint = "https://bookscatalogueapi.azurewebsites.net/api/books/";
 
         public ReviewController() {
         }
